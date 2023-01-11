3d29b52ca3eb110c6a9754f6c1e51a96452e0782
levpolshkov <polshkov.lv@gmail.com>
2023-01-10 18:20:07 -0600

    Load nodes by graphql and render

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,39 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonPage } from '@ionic/react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [nodes, setNodes] = useState<{ node_id: number; node_type: string }[]>(
+    [],
+  );
+
+  useEffect(() => {
+    axios
+      .post('http://localhost:3000/graphql', {
+        query: `
+          query {
+            nodes {
+              node_id
+              node_type
+            }
+          }
+        `,
+      })
+      .then((response) => setNodes(response.data.data.nodes));
+  }, []);
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <div className="nodes">
+          {nodes.map((node, index) => (
+            <div key={index} className="node">
+              {node.node_type}
+            </div>
+          ))}
+        </div>
       </IonContent>
     </IonPage>
   );
