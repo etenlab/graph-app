20794f22f20b0aa8750342679635599b16b6e9e8
levpolshkov <polshkov.lv@gmail.com>
2023-01-17 16:03:54 -0600

    Show relationships nodes. Allow navigate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,12 @@ import {
   IonHeader,
   IonLabel,
   IonInput,
-  IonLoading,
 } from '@ionic/react';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
+import classnames from 'classnames';
 
+import { Loader } from '../components/Loader';
 import './Home.css';
 
 type Node = {
@@ -37,6 +38,8 @@ type Relationship = {
   from_node_id: number;
   to_node_id: number;
   propertyKeys: RelationshipPropertyKey[];
+  fromNode: Node;
+  toNode: Node;
 };
 
 type RelationshipPropertyKey = {
@@ -48,19 +51,67 @@ type RelationshipPropertyValue = {
   property_value: PropertyValue;
 };
 
+const nodeFields = `
+  node_id
+  node_type
+  propertyKeys {
+    property_key
+    values {
+      property_value {
+        value
+      }
+    }
+  }
+`;
+
+const relationshipFields = `
+  relationship_type
+  from_node_id
+  to_node_id
+  propertyKeys {
+    property_key
+    values {
+      property_value {
+        value
+      }
+    }
+  }
+  fromNode {
+    ${nodeFields}
+  }
+  toNode {
+    ${nodeFields}
+  }
+`;
+
 export default function Home() {
   const [selectedNodeId, setSelectedNodeId] = useState<number | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  if (!selectedNodeId) {
-    return <SearchScreen onNodeIdSelect={setSelectedNodeId} />;
-  }
-
-  return <NodeDetailsScreen nodeId={selectedNodeId} />;
+  return (
+    <>
+      {isLoading && <Loader />}
+      {!selectedNodeId ? (
+        <SearchScreen
+          setIsLoading={setIsLoading}
+          onNodeIdSelect={setSelectedNodeId}
+        />
+      ) : (
+        <NodeDetailsScreen
+          setIsLoading={setIsLoading}
+          nodeId={selectedNodeId}
+          onNodeIdSelect={setSelectedNodeId}
+        />
+      )}
+    </>
+  );
 }
 
 function SearchScreen({
+  setIsLoading,
   onNodeIdSelect,
 }: {
+  setIsLoading(value: boolean): void;
   onNodeIdSelect(node_id: number): void;
 }) {
   const [input, setInput] = useState('');
@@ -72,27 +123,20 @@ function SearchScreen({
       setNodes([]);
       return;
     }
+    setIsLoading(true);
     axios
       .post('http://localhost:3000/graphql', {
         query: `
           query {
             nodesBySearch(search: "${search}") {
-              node_id
-              node_type
-              propertyKeys {
-                property_key
-                values {
-                  property_value {
-                    value
-                  }
-                }
-              }
+              ${nodeFields}
             }
           }
         `,
       })
-      .then((response) => setNodes(response.data.data.nodesBySearch));
-  }, [search]);
+      .then((response) => setNodes(response.data.data.nodesBySearch))
+      .finally(() => setIsLoading(false));
+  }, [search, setIsLoading]);
 
   return (
     <IonPage>
@@ -132,38 +176,27 @@ function SearchScreen({
   );
 }
 
-function NodeDetailsScreen({ nodeId }: { nodeId: number }) {
+function NodeDetailsScreen({
+  setIsLoading,
+  nodeId,
+  onNodeIdSelect,
+}: {
+  setIsLoading(value: boolean): void;
+  nodeId: number;
+  onNodeIdSelect(node_id: number): void;
+}) {
   const [node, setNode] = useState<null | Node>(null);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     axios
       .post('http://localhost:3000/graphql', {
         query: `
           query {
             node(id: ${nodeId}) {
-              node_id
-              node_type
-              propertyKeys {
-                property_key
-                values {
-                  property_value {
-                    value
-                  }
-                }
-              }
+              ${nodeFields}
               relationships {
-                relationship_type
-                from_node_id
-                to_node_id
-                propertyKeys {
-                  property_key
-                  values {
-                    property_value {
-                      value
-                    }
-                  }
-                }
+                ${relationshipFields}
               }
             }
           }
@@ -171,29 +204,40 @@ function NodeDetailsScreen({ nodeId }: { nodeId: number }) {
       })
       .then((response) => setNode(response.data.data.node))
       .finally(() => setIsLoading(false));
-  }, [nodeId]);
+  }, [nodeId, setIsLoading]);
 
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className="inner">
-          {isLoading && (
-            <IonLoading isOpen showBackdrop={false} cssClass="loader" />
-          )}
           {node && (
             <>
               <IonLabel className="label">selected node</IonLabel>
-              <NodeItem node={node} highlighted />
+              <NodeItem node={node} warning />
               {node.relationships && node.relationships.length > 0 && (
                 <div style={{ marginTop: 15 }}>
                   <IonLabel className="label">relationships</IonLabel>
                   <div>
-                    {node.relationships.map((relationship, index) => (
-                      <RelationshipItem
-                        key={index}
-                        relationship={relationship}
-                      />
-                    ))}
+                    {node.relationships.map((relationship, index) => {
+                      const relationshipNode =
+                        relationship.fromNode.node_id !== node.node_id
+                          ? relationship.fromNode
+                          : relationship.toNode;
+
+                      return (
+                        <Fragment key={index}>
+                          <RelationshipItem relationship={relationship} />
+                          <NodeItem
+                            node={relationshipNode}
+                            warning
+                            showRelation
+                            onClick={() =>
+                              onNodeIdSelect(relationshipNode.node_id)
+                            }
+                          />
+                        </Fragment>
+                      );
+                    })}
                   </div>
                 </div>
               )}
@@ -207,40 +251,23 @@ function NodeDetailsScreen({ nodeId }: { nodeId: number }) {
 
 function NodeItem({
   node: { node_type, propertyKeys },
-  highlighted = false,
+  warning = false,
+  showRelation,
   onClick,
 }: {
   node: Node;
-  highlighted?: boolean;
+  warning?: boolean;
+  showRelation?: boolean;
   onClick?: () => void;
 }) {
   return (
-    <div
-      className={[
-        'item',
-        ...(onClick ? ['item--selectable'] : []),
-        ...(highlighted ? ['item--highlighted'] : []),
-      ].join(' ')}
+    <Item
+      warning={warning}
+      type={node_type}
+      showRelation={showRelation}
+      propertyKeys={propertyKeys}
       onClick={onClick}
-    >
-      <div>{node_type}</div>
-      {propertyKeys.length > 0 && (
-        <div className="property-keys">
-          {propertyKeys.map(({ property_key, values }, index) => (
-            <div key={index} className="property-key">
-              <div>{property_key}</div>
-              <div className="property-values">
-                {values.map(({ property_value: { value } }, index) => (
-                  <div key={index} className="property-value">
-                    {value}
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    />
   );
 }
 
@@ -249,9 +276,40 @@ function RelationshipItem({
 }: {
   relationship: Relationship;
 }) {
+  return <Item success type={relationship_type} propertyKeys={propertyKeys} />;
+}
+
+function Item({
+  type,
+  propertyKeys,
+  success = false,
+  warning = false,
+  onClick,
+  showRelation = false,
+}: {
+  type: string;
+  propertyKeys: Node['propertyKeys'] | Relationship['propertyKeys'];
+  success?: boolean;
+  warning?: boolean;
+  onClick?: () => void;
+  showRelation?: boolean;
+}) {
   return (
-    <div className="item item--highlighted">
-      <div>{relationship_type}</div>
+    <div
+      className={classnames('item', {
+        'item--success': success,
+        'item--warning': warning,
+        'item--selectable': onClick,
+        'item--relation': showRelation,
+      })}
+      onClick={onClick}
+    >
+      {showRelation && (
+        <svg viewBox="0 0 50 50" className="item_arrow">
+          <path d="M14 0 L14 36 L42 36 L42 39 L48 35 L42 31 L42 34 L16 34 L16 0 Z" />
+        </svg>
+      )}
+      <div>{type}</div>
       {propertyKeys.length > 0 && (
         <div className="property-keys">
           {propertyKeys.map(({ property_key, values }, index) => (
